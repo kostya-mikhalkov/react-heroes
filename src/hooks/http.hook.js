ce11c05d6cf4ede7c486b93734f99db15c5b8b60
unknown <kostya.mkhalkov@bk.ru>
2024-07-26 15:16:38 +0300

    init

---
+++ src/hooks/http.hook.js
@@ -0,0 +1,48 @@
+import { useCallback } from "react";
+
+export const useHttp = () => {
+    // const [process, setProcess] = useState('waiting');
+
+    const request = useCallback(async (url, method = 'GET', body = null, headers = {'Content-Type': 'application/json'}) => {
+
+        // setProcess('loading');
+
+        try {
+            const response = await fetch(url, {method, body, headers});
+
+            if (!response.ok) {
+                throw new Error(`Could not fetch ${url}, status: ${response.status}`);
+            }
+
+            const data = await response.json();
+
+            return data;
+        } catch(e) {
+            // setProcess('error');
+            throw e;
+        }
+    }, []);
+
+    const requestDelete = async (url, id, method = 'DELETE') => {
+        try {
+            const response = await fetch(`${url}/${id}`, {method});
+            if (!response.ok) {
+                throw new Error(`Could not fetch ${url}, status: ${response.status}`);
+            }
+            const data = await response.json()
+        } catch(e) {
+            throw e;
+        }
+    }
+
+    // const clearError = useCallback(() => {
+        // setProcess('loading');
+    // }, []);
+
+    return {request,
+            requestDelete, 
+            // clearError, 
+            // process, 
+            // setProcess
+        }
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,36 @@
+const initialState = {
+    heroes: [],
+    heroesLoadingStatus: 'idle',
+    filters: [],
+    id: null
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'HEROES_FETCHING':
+            return {
+                ...state,
+                heroesLoadingStatus: 'loading'
+            }
+        case 'HEROES_FETCHED':
+            return {
+                ...state,
+                heroes: action.payload,
+                heroesLoadingStatus: 'idle'
+            }
+        case 'HEROES_FETCHING_ERROR':
+            return {
+                ...state,
+                heroesLoadingStatus: 'error'
+            }
+        case 'HEROES_DELETED':
+            return {
+                ...state,
+                heroes: state.heroes.filter(item => item.id !== action.payload),
+                id: action.payload
+            }
+        default: return state
+    }
+}
+
+export default reducer;
